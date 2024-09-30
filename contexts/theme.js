3b607b387eb3a326d59d46e50fcaaa6d5c5d57a7
Giorgio Delle Grottaglie <giorgiodg@gmail.com>
2024-09-30 16:02:42 +0200

    Fixed syntax for robots

--- contexts/theme.js
+++ contexts/theme.js
@@ -4,8 +4,7 @@ import PropTypes from 'prop-types'
 const ThemeContext = createContext()
 
 const ThemeProvider = ({ children }) => {
-  // const [themeName, setThemeName] = useState('light')
-  const [themeName, setThemeName] = useState('dark')
+  const [themeName, setThemeName] = useState('light')
 
   useEffect(() => {
     setThemeName(
